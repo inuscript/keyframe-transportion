50e79a78d0a6d73417b1523347089adb1aa6aa92
inuscript <inuscript@gmail.com>
2016-09-04 22:31:12 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+
+
+```js
+{
+  opacity: [0.5, 1],
+  transform: ['scale(0.5)', 'scale(1)']
+}
+
+[{
+  opacity: 0.5,
+  transform: 'scale(0.5)'
+}, {
+  opacity: 1,
+  transform: 'scale(1)'
+}]
+
+```
---
+++ test/index.js
@@ -0,0 +1,28 @@
+const keyframeTranspotion = require('../index')
+const assert = require('assert')
+
+describe('keyframeTranspotion', function () {
+  it('default pattern', function () {
+    const result = keyframeTranspotion({
+      opacity: [0.5, 1],
+      transform: ['scale(0.5)', 'scale(1)']
+    })
+    const expect = [
+      { offset: 0, opacity: 0.5, transform: 'scale(0.5)' },
+      { offset: 1, opacity: 1, transform: 'scale(1)' }
+    ]
+    assert.deepEqual(result, expect)
+  })
+  it('calcurate offset', function () {
+    const result = keyframeTranspotion({
+      opacity: [0.5, 2, 1],
+      transform: ['scale(0.5)', 'scale(1)']
+    })
+    const expect = [
+      { offset: 0, opacity: 0.5, transform: 'scale(0.5)' },
+      { offset: 0.5, opacity: 2 },
+      { offset: 1, opacity: 1 , transform: 'scale(1)' },
+    ]
+    assert.deepEqual(result, expect)
+  })
+})
